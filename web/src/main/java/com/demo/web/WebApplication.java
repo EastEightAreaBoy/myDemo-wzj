47c81263b1773a85dfb65d8170cefb7df408e996
wangzj28 <wangzj28@chinatelecom.cn>
2022-09-20 18:21:07 +0800

    security

---
+++ web/src/main/java/com/demo/web/WebApplication.java
@@ -0,0 +1,16 @@
+package com.demo.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author HP
+ */
+@SpringBootApplication
+public class WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+
+}
